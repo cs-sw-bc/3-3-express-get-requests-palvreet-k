9f5e912d4d7708caa88677ef9b6386dcb4ea1822
palvreet-k <kaurpalvreet@gmail.com>
2025-11-15 00:46:54 -0500

    front and back end assignment

--- server/app.js
+++ server/app.js
@@ -5,34 +5,58 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-const HARD_CODED_JSON = {
-  results: [
-    {
-      name: {
-        title: "Miss",
-        first: "Jennie",
-        last: "Nichols",
-      },
-      email: "jennie.nichols@example.com",
-      dob: {
-        age: 30,
-      },
-    },
-  ],
-};
+// const HARD_CODED_JSON = {
+//   results: [
+//     {
+//       name: {
+//         title: "Miss",
+//         first: "Jennie",
+//         last: "Nichols",
+//       },
+//       email: "jennie.nichols@example.com",
+//       dob: {
+//         age: 30,
+//       },
+//     },
+//   ],
+// };
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Users API!");
 });
 
 app.get("/users", (req, res) => {
-    // For now, we're using hardcoded JSON data
-    let randomUser = HARD_CODED_JSON.results[0];
-    res.json(randomUser);
+  // For now, we're using hardcoded JSON data
+  // let randomUser = HARD_CODED_JSON.results[0];
+  // res.json(randomUser);
 
-    // TODO: Replace the hardcoded JSON with a real API request to get a random user
-    // For example, you might use the `axios` library to make a GET request to the API endpoint
-  });
+  // TODO: Replace the hardcoded JSON with a real API request to get a random user
+  // For example, you might use the `axios` library to make a GET request to the API endpoint
+
+  fetch("https://randomuser.me/api")
+    .then(res => res.json())
+    .then(data => {
+      const user = data.results[0];
+      res.json(user);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: "Failed to fetch user" });
+    });
+
+});
+
+
+app.get("/health/:status", async (req, res) => {
+  const status = req.params.status;
+  if (status === "good") {
+    res.status(200).json({ status: "Health is good" });
+  }
+  else {
+    res.status(500).json({ status: "Health is bad" });
+  }
+
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
